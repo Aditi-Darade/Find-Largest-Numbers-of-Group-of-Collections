47589eb93298a4540e964ba5d10ca99b46b6f0f5
Aditi <aditidarade1@gmail.com>
2024-10-21 13:33:31 -0500

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public List<int> FindLargest(List<List<int>> collections)
+        {
+            /*
+            //-------------------------------First Way---------------------------------------
+
+            List<int> tempListCollection = new List<int>();
+
+            foreach (List<int> lists in collections)
+            {
+                lists.Sort();
+                int count = lists.Count(); 
+                int LastNumber = lists[count - 1];
+                tempListCollection.Add(LastNumber);
+            }
+            return tempListCollection;
+            */
+            //-------------------------------Second Way---------------------------------------
+            ///*
+            List<int> list = new List<int>();
+
+            foreach (List<int> lists in collections)
+            {
+                int number = 0;
+                foreach (int item in lists)
+                {
+                    if (number < item)
+                    {
+                        number = item;
+                    }
+                }
+                list.Add(number);
+            }
+            return list;
+            //*/
+
+        }
+    }
+}
---
+++ Sec22_Assign47_FindLargestNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec22_Assign47_FindLargestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Class1 class1 = new Class1();
+
+            
+            List<int> result = new List<int>();
+
+            result = class1.FindLargest(new List<List<int>>() 
+            {
+                new List<int>( ) { 67, 100, 23 },
+                new List<int>( ) { 80, 99, 750, 99 },
+                new List<int>( ) { 888, 333, 9898 } 
+            });  //should return new List<int>( ) { 100, 750, 9898 }
+
+
+            foreach ( int item in result)
+            {
+                Console.WriteLine( item );
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
